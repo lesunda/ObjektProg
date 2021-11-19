ca819f353ed6128c72e26605c819b623e54cc212
lesunda <91281608+lesunda@users.noreply.github.com>
2021-11-19 23:55:13 +0200

    Create studentas.h

---
+++ studentas.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+struct studentas {
+	std::string Vard, Pav;
+	std::vector<int> paz = {};
+	int egz;
+	float GALPAZ = 0;
+};
