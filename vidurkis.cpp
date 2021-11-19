ce8238bfb92af9adf99f8742ce3b03fc0e2532e4
lesunda <91281608+lesunda@users.noreply.github.com>
2021-11-19 22:26:19 +0200

    Delete vidurkis.cpp

--- vidurkis.cpp
+++
@@ -1,8 +0,0 @@
-#include "vidurkis.h"
-#include <numeric>
-
-double vidurkis(vector<double> namuDarbuPazymiai) {
-    double average;
-    average = accumulate(namuDarbuPazymiai.begin(), namuDarbuPazymiai.end(), 0) / namuDarbuPazymiai.size();
-    return average;
-}
