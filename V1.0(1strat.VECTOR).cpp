64f6ba95062177bbace9114ea361308e744c1e22
lesunda <91281608+lesunda@users.noreply.github.com>
2021-11-19 22:01:51 +0200

    Update V1.0(1strat.VECTOR).cpp

--- V1.0(1strat.VECTOR).cpp
+++ V1.0(1strat.VECTOR).cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <numeric>
 #include <chrono>
+#include <algorithm>
 
 using namespace std;
 
@@ -31,7 +32,7 @@ vector<int> automatinis_pazymiu_generavimas_kiekv_stud(int pazymiu_skaicius) {
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------
 
-double galutinio_pazymio_skaiciavimas(vector<int>& kiekvieno_mok_pazymiai) {
+double galutinio_pazymio_skaiciavimas_su_vidurkiu(vector<int>& kiekvieno_mok_pazymiai) {
 	studentas Eil;
 	Eil.GALPAZ = 0.4 * accumulate(kiekvieno_mok_pazymiai.begin(), kiekvieno_mok_pazymiai.end(), 0) / kiekvieno_mok_pazymiai.size() + 0.6 * random_pazymiu_sudarymas();
 	return Eil.GALPAZ;
@@ -39,42 +40,125 @@ double galutinio_pazymio_skaiciavimas(vector<int>& kiekvieno_mok_pazymiai) {
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------
 
-int pagrindinio_failo_kurimas(vector<int>& studentu_skaicius) {
-	int studentu_kiekis;
-	cout << "Iveskite skaiciu keliu studentu rezultatus noresite matyti:  " << endl;
-	cin >> studentu_kiekis;
-	cout << "------------------------------------------------------------------------------------------------" << endl;
-
-
-	string pagr_failo_pav = "Visi_Studentai" + to_string(studentu_kiekis) + ".txt";
-
-
-	auto startas1 = chrono::high_resolution_clock::now(); //STARTAS//
-
-
-	ofstream pagr_failo_spausd(pagr_failo_pav);
-	vector<int> mokinio_pazymiai;
-
-	pagr_failo_spausd << setw(20) << left << "Vardenis"
-		<< setw(20) << left << "Pavardenis"
-		<< setw(20) << left << "Galutinis(Vid.)" << endl;
-
-
-
-	for (int s = 1; s <= studentu_kiekis; s = s + 1) {
-		mokinio_pazymiai = automatinis_pazymiu_generavimas_kiekv_stud(5);
-		pagr_failo_spausd << setw(20) << "Vardenis" + to_string(s) << setw(20) << "Pavardenis" + to_string(s) << setw(20) << galutinio_pazymio_skaiciavimas(mokinio_pazymiai) << endl;
-		mokinio_pazymiai.clear();
-
+double medianos_skaiciavimas(vector<int> kiekvieno_mok_pazymiai) {
+	studentas Eil;
+	int Dydis;
+	Dydis = kiekvieno_mok_pazymiai.size();
+	if (Dydis == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		sort(kiekvieno_mok_pazymiai.begin(), kiekvieno_mok_pazymiai.end());
+		//for (int i = 0; i < Dydis; i++) {
+		//}
+		if (Dydis % 2 == 0)
+		{
+			//return (kiekvieno_mok_pazymiai[Dydis / 2 - 1] + kiekvieno_mok_pazymiai[Dydis / 2.0]) / 2.0;
+			return Eil.GALPAZ = 0.4 * ((kiekvieno_mok_pazymiai[Dydis / 2 - 1] + kiekvieno_mok_pazymiai[Dydis / 2.0]) / 2.0) + 0.6 * random_pazymiu_sudarymas();
+		}
+		else
+		{
+			//return  kiekvieno_mok_pazymiai[Dydis / 2.0];
+			return Eil.GALPAZ = 0.4 * (kiekvieno_mok_pazymiai[Dydis / 2.0]) + 0.6 * random_pazymiu_sudarymas();
+		}
 	}
 
-	auto endas1 = chrono::high_resolution_clock::now();  //PABAIGA//
-	chrono::duration<double> skirtumas1 = endas1 - startas1;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
+int pagrindinio_failo_kurimas(vector<int>& studentu_skaicius) {
+	int studentu_kiekis;
+	string pasirinkimas;
 
 
-	cout << to_string(studentu_kiekis) + " studentu pagrindinis failo kurimas uztruko " << skirtumas1.count() << " sekundziu." << endl;
+	cout << "Iveskite skaiciu keliu studentu rezultatus noresite matyti:  " << endl;
+	cin >> studentu_kiekis;
+	cout << "Iveskite kaip noresite skaiciuoti galutini pazymi. Jeigu su mediana rasykite M. Su vidurkiu rasykite V." << endl;
+	cin >> pasirinkimas;
 
-	return studentu_kiekis;
+
+	while (pasirinkimas != "m" && pasirinkimas != "M" && pasirinkimas != "v" && pasirinkimas != "V") {
+		std::cout << "Iveskite tai, ko prasoma.\n";
+		std::cin >> pasirinkimas;
+	}
+
+	if (pasirinkimas == "v" || pasirinkimas == "V") {
+
+		cout << "------------------------------------------------------------------------------------------------" << endl;
+
+
+		string pagr_failo_pav = "Visi_Studentai" + to_string(studentu_kiekis) + ".txt";
+
+
+		auto startas1 = chrono::high_resolution_clock::now(); //STARTAS//
+
+
+		ofstream pagr_failo_spausd(pagr_failo_pav);
+		vector<int> mokinio_pazymiai;
+
+		pagr_failo_spausd << setw(20) << left << "Vardenis"
+			<< setw(20) << left << "Pavardenis"
+			<< setw(20) << left << "Galutinis(Vid.)" << endl;
+
+
+
+		for (int s = 1; s <= studentu_kiekis; s = s + 1) {
+			mokinio_pazymiai = automatinis_pazymiu_generavimas_kiekv_stud(5);
+			pagr_failo_spausd << setw(20) << "Vardenis" + to_string(s) << setw(20) << "Pavardenis" + to_string(s) << setw(20) << galutinio_pazymio_skaiciavimas_su_vidurkiu(mokinio_pazymiai) << endl;
+			mokinio_pazymiai.clear();
+
+		}
+
+		auto endas1 = chrono::high_resolution_clock::now();  //PABAIGA//
+		chrono::duration<double> skirtumas1 = endas1 - startas1;
+
+
+		cout << to_string(studentu_kiekis) + " studentu pagrindinis failo kurimas uztruko " << skirtumas1.count() << " sekundziu." << endl;
+
+		return studentu_kiekis;
+	}
+
+	if (pasirinkimas == "m" || pasirinkimas == "M") {
+		cout << "------------------------------------------------------------------------------------------------" << endl;
+
+
+		string pagr_failo_pav = "Visi_Studentai" + to_string(studentu_kiekis) + ".txt";
+
+
+		auto startas1 = chrono::high_resolution_clock::now(); //STARTAS//
+
+
+		ofstream pagr_failo_spausd(pagr_failo_pav);
+		vector<int> mokinio_pazymiai;
+
+		pagr_failo_spausd << setw(20) << left << "Vardenis"
+			<< setw(20) << left << "Pavardenis"
+			<< setw(20) << left << "Galutinis(Med.)" << endl;
+
+
+
+		for (int s = 1; s <= studentu_kiekis; s = s + 1) {
+			mokinio_pazymiai = automatinis_pazymiu_generavimas_kiekv_stud(5);
+			pagr_failo_spausd << setw(20) << "Vardenis" + to_string(s) << setw(20) << "Pavardenis" + to_string(s) << setw(20) << medianos_skaiciavimas(mokinio_pazymiai) << endl;
+			mokinio_pazymiai.clear();
+
+		}
+
+		auto endas1 = chrono::high_resolution_clock::now();  //PABAIGA//
+		chrono::duration<double> skirtumas1 = endas1 - startas1;
+
+
+		cout << to_string(studentu_kiekis) + " studentu pagrindinis failo kurimas uztruko " << skirtumas1.count() << " sekundziu." << endl;
+
+		return studentu_kiekis;
+	}
+
+
+
+
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------
